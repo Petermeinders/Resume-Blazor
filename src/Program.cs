41e3792ebf05de75c37e96bd755534628ed6099b
Peter <52257026+Petermeinders@users.noreply.github.com>
2025-06-09 16:18:28 -0400

    Add Swapi models and service for API integration

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,7 @@ using Resume_Blazor;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<SwapiService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredToast();
 await builder.Build().RunAsync();
