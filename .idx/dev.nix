af76823bb045f5ce6f2093138f83fc72bbd856db
petermeinder <petermeinder@gmail.com>
2025-06-23 16:17:50 +0000

    Added Budget Buddy ADD functionality to firebase. Still working on EDIT and DELETE functionality. Need to move appsettings into github still.

---
+++ .idx/dev.nix
@@ -0,0 +1,57 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    pkgs.dotnet-sdk_8
+    pkgs.dotnet-sdk
+    # pkgs.nodePackages.nodemon
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    internal.android-emulator.enable = false;
+    extensions = [
+      # "vscodevim.vim"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+ manager = "web";
+          # and show it in IDX's web preview panel
+          command = ["dotnet" "run" "--project" "src/Resume-Blazor.csproj"];
+          env = {
+            # Environment variables to set for your server
+            PORT = "6001";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
